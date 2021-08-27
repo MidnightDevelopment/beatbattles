397762f34e0d9500e4d6c2359bb1e2d8ace79517
MidnightDevelopment <eastmusicoffical@gmail.com>
2021-08-27 19:36:22 -0400

    things are finally working

--- client/src/pages/create_battle/Create.js
+++ client/src/pages/create_battle/Create.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import moment from 'moment';
-import {Form, Button, Typography, Row, DatePicker} from 'antd';
+import {Form, Button, Typography, Row, DatePicker, message} from 'antd';
 import '../MainPage.less';
 import { withRouter } from 'react-router-dom';
 import routes from "../routes";
+import {state} from "pg/lib/native/query";
 
 const { RangePicker } = DatePicker;
 const { Title } = Typography;
@@ -12,13 +13,52 @@ class Create extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            startHour: 0,
-            startMinute: 0
+            startDate: null,
+            endDate: null,
         }
+        this.onCalendarChange = this.onCalendarChange.bind(this);
         this.disabledDate = this.disabledDate.bind(this);
     }
 
-    handleSubmit = () => {
+    makeid(length) {
+        let result = '';
+        let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+        let charactersLength = characters.length;
+        for (let i = 0; i < length; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
+
+
+    onCalendarChange (dates, dateStrings, info) {
+        console.log('hello')
+        console.log(dateStrings)
+
+        this.setState({
+            startDate: dateStrings[0],
+            endDate: dateStrings[1]
+        })
+    }
+
+    handleSubmit  = async e => {
+        if (this.state.startDate === null || this.state.endDate === null) {
+            message.error('Range is invalid!');
+            return;
+        }
+
+        const body = {
+            "id": this.makeid(6),
+            "startDate": this.state.startDate,
+            "endDate": this.state.endDate
+        };
+
+        const response = await fetch("http://localhost:5000/battles", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body)
+        });
+
         this.props.history.push(routes.battleCreated);
     }
 
@@ -44,6 +84,7 @@ class Create extends React.Component {
                         </Form.Item>
                         <Form.Item>
                             <RangePicker
+                                onCalendarChange={this.onCalendarChange}
                                 disabledDate={this.disabledDate}
                                 showTime={{
                                     hideDisabledOptions: true,
